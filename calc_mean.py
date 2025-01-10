3d5e6413c41311a1dd13ed8e1e94029fc7f1f932
gavrielhan <102911767+gavrielhan@users.noreply.github.com>
2025-01-10 09:47:48 +0100

    new chnages

--- calc_mean.py
+++ calc_mean.py
@@ -3,18 +3,23 @@ import os
 import numpy as np
 os.chdir('/Users/gavrielhannuna/lrgb_source_code/training_logs')
 # List of seed numbers
-seeds = [2025, 42, 5, 123,18]
-
+#seeds = [2025, 42, 5, 123,18]
+seeds = [42, 123,18,2025]
+LRGB_d = 'Peptides-func'
 # Path to the JSON files
-file_pattern = "seed_{seed_num}_logs.json"
+if LRGB_d =='Peptides-struct':
+    file_pattern = "seed_{seed_num}_logs.json"
+else:
+    file_pattern = "seed_{seed_num}_logs_func.json"
 
 
-# Key to calculate the statistics
 val_key = "val_mae"  # Key for validation metric
 test_key = "test_mae"  # Key for test metric
+
+
 # Store the averages for the last 10 epochs for each seed
 # Store the selected test_mae values for each seed
-test_mae_values = []
+test_values = []
 
 for seed in seeds:
     # Load the JSON file
@@ -23,14 +28,21 @@ for seed in seeds:
         logs = json.load(f)
 
     # Find the epoch with the lowest val_mae
-    min_val_mae_entry = min(logs, key=lambda entry: entry[val_key])
+    if LRGB_d == 'Peptides-struct':
+        min_val_mae_entry = min(logs, key=lambda entry: entry[val_key])
+        # Get the test_mae corresponding to the lowest val_mae
+        test_values.append(min_val_mae_entry[test_key])
+    else:
+        # Find the epoch with the highest val_ap
+        max_val_ap_entry = max(logs, key=lambda entry: entry[val_key])
+        # Get the test_mae corresponding to the lowest val_mae
+        test_values.append(max_val_ap_entry[test_key])
 
-    # Get the test_mae corresponding to the lowest val_mae
-    test_mae_values.append(min_val_mae_entry[test_key])
+
 
 # Calculate the final mean and standard deviation
-final_mean = np.mean(test_mae_values)
-final_std = np.std(test_mae_values)
+final_mean = np.mean(test_values)
+final_std = np.std(test_values)
 
 # Calculate SD as a percentage of the mean
 sd_percentage = (final_std / final_mean) * 100
